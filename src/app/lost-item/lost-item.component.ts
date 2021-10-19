fed48df213f026fe7153dab7d46d8022ce02eb87
Manav Bhosale <80413980+MANAVBHOSALE@users.noreply.github.com>
2021-10-19 14:13:48 +0530

    Add files via upload

---
+++ src/app/lost-item/lost-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lost-item',
+  templateUrl: './lost-item.component.html',
+  styleUrls: ['./lost-item.component.css']
+})
+export class LostItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
