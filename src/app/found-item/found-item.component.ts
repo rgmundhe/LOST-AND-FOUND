7418bd76e7717ab54878684c885ee9862a69c885
Manav Bhosale <80413980+MANAVBHOSALE@users.noreply.github.com>
2021-10-19 14:09:11 +0530

    Add files via upload

---
+++ src/app/found-item/found-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-found-item',
+  templateUrl: './found-item.component.html',
+  styleUrls: ['./found-item.component.css']
+})
+export class FoundItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
